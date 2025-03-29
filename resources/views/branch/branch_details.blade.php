cda5e0f71ad8ec7ed9cc5f409ee9a67bfbd5fab7
BENEDICT <svcdesk3.mis.lyrgroup@gmail.com>
2025-03-29 11:23:30 +0800

    branch remove functionality

--- resources/views/branch/branch_details.blade.php
+++ resources/views/branch/branch_details.blade.php
@@ -80,12 +80,7 @@
 
                 </div>
                 <div class="modal-footer">
-                    <button onclick="history.back()" class="btn btn-secondary" type="button"> <svg
-                            xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor"
-                            class="bi bi-reply-fill" viewBox="0 0 16 16">
-                            <path
-                                d="M5.921 11.9 1.353 8.62a.72.72 0 0 1 0-1.238L5.921 4.1A.716.716 0 0 1 7 4.719V6c1.5 0 6 0 7 8-2.5-4.5-7-4-7-4v1.281c0 .56-.606.898-1.079.62z" />
-                        </svg> Back </button>
+                    <button onclick="history.back()" class="btn btn-secondary" type="button">Back </button>
                 </div>
 
             </div>
