d31c8d9b48e5ea107f76f5fdb82fc45183a48389
Benedict Lu <svcdesk3.mis.lyrgroup@gmail.com>
2026-02-05 13:38:05 +0800

    add inventory adjustment

--- database/migrations/2026_01_31_065427_create_inventory_adjustments_table.php
+++ database/migrations/2026_01_31_065427_create_inventory_adjustments_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->enum('adjustment_type', ['INCREASE', 'DECREASE']);
             $table->foreignId('branch_id')->nullable()->constrained('branches')->onDelete('cascade')->onUpdate('no action');
             $table->enum('status', ['DRAFT','PENDING', 'APPROVED', 'REJECTED'])->default('PENDING');
+            $table->enum('category', ['ITEM', 'RECIPE'])->default('ITEM');
             $table->foreignId('created_by')->nullable()->constrained('employees')->onDelete('no action')->onUpdate('no action');
             $table->foreignId('approved_by')->nullable()->constrained('employees')->onDelete('no action')->onUpdate('no action');
             $table->enum('reason', ['DAMAGED', 'EXPIRED', 'LOSS', 'EXCESS','INVENTORY_COUNT']);
